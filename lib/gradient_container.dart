4609c6918dd66ed5a0faa2cefc66ff97c54ddf1d
larry <kaihsun_chen@9splay.com>
2025-09-04 01:28:17 +0800

    13 Introducing Stateful Widget
    狀態組件，狀態本體、State陣列extends本體

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:projectRollDiceApp/styled_text.dart';
 
+import 'package:projectRollDiceApp/dice_roller.dart';
+
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -15,10 +17,6 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  void rollDice() {
-    // ...
-  }
-
   @override
   Widget build(context) {
     return Container(
@@ -29,26 +27,7 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/images/dice-2.png', width: 200),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(
-                //   top: 20,
-                // ),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
-      ),
+      child: Center(child: DiceRoller()),
     );
   }
 }
